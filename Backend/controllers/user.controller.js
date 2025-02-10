93934d2c03cdb22b8ce3aa2089e62c026fa2d1d4
Uzumaki-tushar <tusharsourav01@gamil.com>
2025-02-10 23:52:07 +0530

    created profile update controller

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -76,7 +76,7 @@ export const login= async (req,res)=>{
 
         // generate token
         const tokenData={
-            userId:user._id;
+            userId:user._id,
         }
         const token=await jwt.sign(tokenData,process.env.JWT_SECRET,{expiresIn:"1d"});
 
@@ -118,6 +118,59 @@ export const logout= async (req,res)=>{
     catch(error){
         res.status(500).josn({
             message:"Server Error logout",
+            success:false,
+        })
+    }
+}
+
+export const updateProfile= async(req,res)=>{
+    try{
+        const {fullname,email,phoneNumber,bio,skills}=req.body;
+        const file=req.file;
+        if(!fullname || !email || !phoneNumber || !bio || !skills){
+            return res.status(404).josn({
+                message:"Missing required fields",
+                success: false,
+            })
+        }
+
+        // cloudinary upload
+        const skillsArray=skills.split(',');
+        const userId=req.id; // middleware
+        let user= await User.findById(userId);
+        if(!user){
+            return res.status(404).json({
+                message:"User not found",
+                success:false,
+            })
+        }
+        user.fullname=fullname;
+        user.email=email;
+        user.phoneNumber=phoneNumber;
+        user.bio=bio;
+        user.skills=skillsArray;
+        // resume
+        await user.save();
+
+        user={
+            _id:user,_id,
+            fullname:user.fullname,
+            email:user.email,
+            phoneNumber:user.phoneNumber,
+            role:user.role,
+            profile: user.profile,
+        }
+
+        return res.status(200).json({
+            message:"Profile updated successfully",
+            user,
+            success:true,
+        })
+
+    }
+    catch(error){
+        res.status(500).josn({
+            message:"Server Error profile update",
             success:false,
         })
     }
