f89e306d40b8d123e88e847f0f77c8e2083a6877
Uzumaki-tushar <tusharsourav01@gamil.com>
2025-02-15 05:34:02 +0530

    created frontend folder structure using vite sandscn ui and tailwind css

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from './components/ui/button';
+
+const App = () => {
+  return (
+    <h1 className='text-red-500'>Job Portal</h1>
+    // <Button></Button>
+  )
+}
+
+export default App
